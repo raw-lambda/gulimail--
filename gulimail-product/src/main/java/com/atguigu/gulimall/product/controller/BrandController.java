48beaf04fae561291b8607a6b693300d95188823
lambda <shujuxiaosuicong@gmail.com>
2022-03-31 19:20:12 +0800

    2022/3/31

--- gulimail-product/src/main/java/com/atguigu/gulimall/product/controller/BrandController.java
+++ gulimail-product/src/main/java/com/atguigu/gulimall/product/controller/BrandController.java
@@ -2,15 +2,15 @@ package com.atguigu.gulimall.product.controller;
 
 import com.atguigu.common.utils.PageUtils;
 import com.atguigu.common.utils.R;
+import com.atguigu.common.vaild.AddGroup;
+import com.atguigu.common.vaild.UpdateGroup;
 import com.atguigu.gulimall.product.entity.BrandEntity;
 import com.atguigu.gulimall.product.service.BrandService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.validation.BindingResult;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
 
-import javax.validation.Valid;
 import java.util.Arrays;
-import java.util.HashMap;
 import java.util.Map;
 
 
@@ -52,20 +52,21 @@ public class BrandController {
      * 保存
      */
     @RequestMapping("/save")
-    public R save(@Valid @RequestBody BrandEntity brand, BindingResult result){
-        if (result.hasErrors()){
-            Map<String, String> map = new HashMap<>();
-            result.getFieldErrors().forEach((item) -> {
-                String message = item.getDefaultMessage();
-                String field = item.getField();
-                map.put(field, message);
-            });
-            return R.error(400, "提交的数据不合法").put("data", map);
-        } else {
-            brandService.save(brand);
-        }
+    public R save(@Validated({AddGroup.class}) @RequestBody BrandEntity brand/*, BindingResult result*/){
+//        if (result.hasErrors()){
+//            Map<String, String> map = new HashMap<>();
+//            result.getFieldErrors().forEach((item) -> {
+//                String message = item.getDefaultMessage();
+//                String field = item.getField();
+//                map.put(field, message);
+//            });
+//            return R.error(400, "提交的数据不合法").put("data", map);
+//        } else {
+//            brandService.save(brand);
+//        }
 
 
+        brandService.save(brand);
         return R.ok();
     }
 
@@ -73,7 +74,7 @@ public class BrandController {
      * 修改
      */
     @RequestMapping("/update")
-    public R update(@RequestBody BrandEntity brand){
+    public R update(@Validated({UpdateGroup.class}) @RequestBody BrandEntity brand){
 		brandService.updateById(brand);
 
         return R.ok();
--- gulimail-product/src/main/java/com/atguigu/gulimall/product/entity/BrandEntity.java
+++ gulimail-product/src/main/java/com/atguigu/gulimall/product/entity/BrandEntity.java
@@ -1,5 +1,7 @@
 package com.atguigu.gulimall.product.entity;
 
+import com.atguigu.common.vaild.AddGroup;
+import com.atguigu.common.vaild.UpdateGroup;
 import com.baomidou.mybatisplus.annotation.TableId;
 import com.baomidou.mybatisplus.annotation.TableName;
 
@@ -28,18 +30,20 @@ public class BrandEntity implements Serializable {
 	/**
 	 * 品牌id
 	 */
+	@NotNull(message = "修改时id不能为空", groups = {UpdateGroup.class})
+	@Null(message = "新增时不能指定id", groups = {AddGroup.class})
 	@TableId
 	private Long brandId;
 	/**
 	 * 品牌名
 	 */
-	@NotBlank(message = "品牌名必须提交")
+	@NotBlank(message = "品牌名必须提交", groups = {AddGroup.class, UpdateGroup.class})
 	private String name;
 	/**
 	 * 品牌logo地址
 	 */
-	@NotEmpty
-	@URL(message = "logo必须是一个合法的url地址")
+	@NotEmpty(groups = {AddGroup.class})
+	@URL(message = "logo必须是一个合法的url地址", groups = {AddGroup.class, UpdateGroup.class})
 	private String logo;
 	/**
 	 * 介绍
@@ -52,13 +56,13 @@ public class BrandEntity implements Serializable {
 	/**
 	 * 检索首字母
 	 */
-	@NotEmpty
+	@NotEmpty(groups = {AddGroup.class, UpdateGroup.class})
 	@Pattern(regexp = "/^[a-zA-Z]$/", message = "检索首字母必须是一个字母")
 	private String firstLetter;
 	/**
 	 * 排序
 	 */
-	@NotNull
+	@NotNull(groups = {AddGroup.class, UpdateGroup.class})
 	@Min(value = 0, message = "排序必须大于等于0")
 	private Integer sort;
 
